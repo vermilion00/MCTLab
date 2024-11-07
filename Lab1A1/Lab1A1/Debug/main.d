6a31bb99bbbd23363708926eb79a611824b5d857
vermilion00 <lucaspengler0@gmail.com>
2024-11-07 21:41:54 +0100

    Finished Lab 1, added blocking debounce

--- Lab1A1/Lab1A1/Debug/main.d
+++ Lab1A1/Lab1A1/Debug/main.d
@@ -13,7 +13,12 @@ main.d main.o: .././main.c \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\portpins.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\common.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay_basic.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\math.h .././lcd.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\pgmspace.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stddef.h
 
 c:\program\ files\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\xc.h:
 
@@ -44,3 +49,15 @@ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\common.h:
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h:
 
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay_basic.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\math.h:
+
+.././lcd.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\pgmspace.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stddef.h:
--- Lab1A1/Lab1A1/main.c
+++ Lab1A1/Lab1A1/main.c
@@ -6,14 +6,24 @@
  */ 
 
 #include <xc.h>
+#include <util/delay.h>
+#include "lcd.h"
+
+#define DEBOUNCE 10
+
+#ifndef F_CPU
+	#define F_CPU 16000000UL
+#endif
 
 void aufgabe1(void);
 void aufgabe2(void);
 void aufgabe3(void);
 
-short count = 0;
-short flag0 = 0;
-short flag7 = 0;
+uint8_t count = 0;
+short flag = 0;
+short debounce = 0;
+
+char TransmitBuffer[10];
 
 
 int main(void)
@@ -32,48 +42,196 @@ int main(void)
 void aufgabe1(void){
 	while(1){
 		//Set the LED port to be equal to the Switch Port
-		/*TODO: Check if the Switch is active low and LED is active high (should be) */
+		/*TODO: Check if the Switch is active low (STK500 docs) and LED is active low (lab docs) (should be) 
+				If not, inversion might be necessary */
 		//PORT D is the Switch port
 		//PORT B is the LED port
 		PORTB = ~PIND;
 	}
 }
 
+#ifndef DEBOUNCE
+//No debounce enabled
 void aufgabe2(void){
 	while(1){
-		/*TODO: Implement debouncing algorithm? Just wait for 5-20 ms after registering a negative flank before actually reading it*/
 		//Check for negative edge on Sw0, since Switches are active low
-		while((~PIND)&(1<<PD0)){
+		while((~PIND) & (1 << PD0)){
 			//only register switch once per keypress
-			if(flag0 == 0){
-				flag0 = 1;
+			if(flag == 0){
+				flag = 1;
 				//Increment the counter
 				count++;
 			}
 		}
 		//reset the key pressed flag
-		flag0 = 0;
+		flag = 0;
 		
 		//Check for negative edge on Sw7
 		while((~PIND)&(1<<PD7)){
 			//only register switch once per keypress
-			if(flag7 == 0){
-				flag7 = 1;
+			if(flag == 0){
+				flag = 1;
 				//decrement the counter
 				count--;
 			}
 		}
 		//reset the flag
-		flag7 = 0;
+		flag = 0;
 		
 		//Output the count on the LEDs
 		/*TODO: Check if inversion is needed */
-		PORTB = ~count;
+		PORTB = count;
 	}
 }
 
 void aufgabe3(void){
+	//Initialize the LCD with invisible cursor
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
+	
 	while(1){
+		//Check for negative edge on Sw0, since Switches are active low
+		while((~PIND) & (1 << PD0)){
+			//only register switch once per keypress
+			if(flag == 0){
+				flag = 1;
+				//Increment the counter
+				count++;
+			}
+		}
+		//reset the key pressed flag
+		flag = 0;
 		
+		//Check for negative edge on Sw7
+		while((~PIND) & (1 << PD7)){
+			//only register switch once per keypress
+			if(flag == 0){
+				flag = 1;
+				//decrement the counter
+				count--;
+			}
+		}
+		//reset the flag
+		flag = 0;
+		
+		//Output the count on the Display
+		//Clear screen
+		lcd_clrscr();
+		//Convert count to String
+		sprintf(TransmitBuffer, "%s", count);
+		//Output count on Display
+		lcd_puts(TransmitBuffer);
 	}
 }
+
+#else
+//Debounce enabled
+void aufgabe2(void){
+	while(1){
+		//Check for negative edge on Sw0, since Switches are active low
+		while((~PIND) & (1 << PD0)){
+			//Wait for 10 ms if switch hasn't been debounced yet
+			if(debounce == 0){
+				_delay_ms(DEBOUNCE);
+				debounce = 1;
+				continue;
+			} else {
+				//only register switch once per keypress
+				if(flag == 0){
+					flag = 1;
+					//Increment the counter
+					count++;
+				}
+			}
+		}
+		//reset the "key pressed" flag
+		flag = 0;
+		//reset the "key debounced" flag
+		debounce = 0;
+		
+		//Check for negative edge on Sw7
+		while((~PIND) & (1 << PD7)){
+			//Wait for 10 ms if switch hasn't been debounced yet
+			if(debounce == 0){
+				_delay_ms(DEBOUNCE);
+				debounce = 1;
+				continue;
+			} else {
+				//only register switch once per keypress
+				if(flag == 0){
+					flag = 1;
+					//Decrement the counter
+					count--;
+				}
+			}
+		}
+		//reset the "key pressed" flag
+		flag = 0;
+		//reset the "key debounced" flag
+		debounce = 0;
+		
+		//Output the count on the LEDs
+		/*TODO: Check if inversion is needed */
+		PORTB = count;
+	}
+}
+
+void aufgabe3(void){
+	//Initialize the LCD with invisible cursor
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
+	
+	while(1){
+		//Check for negative edge on Sw0, since Switches are active low
+		while((~PIND) & (1 << PD0)){
+			//Wait for 10 ms if switch hasn't been debounced yet
+			if(debounce == 0){
+				_delay_ms(DEBOUNCE);
+				debounce = 1;
+				continue;
+			} else {
+				//only register switch once per keypress
+				if(flag == 0){
+					flag = 1;
+					//Increment the counter
+					count++;
+				}
+			}
+		}
+		//reset the "key pressed" flag
+		flag = 0;
+		//reset the "key debounced" flag
+		debounce = 0;
+		
+		//Check for negative edge on Sw7
+		while((~PIND) & (1 << PD7)){
+			//Wait for 10 ms if switch hasn't been debounced yet
+			if(debounce == 0){
+				_delay_ms(DEBOUNCE);
+				debounce = 1;
+				continue;
+			} else {
+				//only register switch once per keypress
+				if(flag == 0){
+					flag = 1;
+					//Decrement the counter
+					count--;
+				}
+			}
+		}
+		//reset the "key pressed" flag
+		flag = 0;
+		//reset the "key debounced" flag
+		debounce = 0;
+		
+		//Output the count on the Display
+		//Clear screen
+		lcd_clrscr();
+		//Convert count to String
+		sprintf(TransmitBuffer, "%s", count);
+		//Output count on Display
+		lcd_puts(TransmitBuffer);
+	}
+}
+#endif
+	
