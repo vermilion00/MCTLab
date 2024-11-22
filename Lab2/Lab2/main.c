b39e2f2a97127a754cde30ab56855f6f9957934e
vermilion00 <lucaspengler0@gmail.com>
2024-11-22 12:19:13 +0100

    Fixed Lab 2 code

--- Lab2/Lab2/main.c
+++ Lab2/Lab2/main.c
@@ -1,10 +1,10 @@
-#include <xc.h>
-#include <util/delay.h>
-
 //Set the CPU frequency, needed for the _delay_ms function
 #ifndef F_CPU
 	#define F_CPU 16000000UL
 #endif
+
+#include <xc.h>
+#include <util/delay.h>
 
 void aufgabe1(void);
 void aufgabe2(void);
@@ -13,49 +13,69 @@ int main(void)
 {
 	//No infinite loop, as those are present in the exercise functions
 	//Set the entire LED register as Output
-    DDRB = 0xFF;
+	DDRB = 0xFF;
+	//Turn off all LEDs
+	PORTB = 0xFF;
 	
-	aufgabe1();
-	//aufgabe2();
+	//aufgabe1();
+	aufgabe2();
 }
 
 void aufgabe1(void){
 	while(1){
 		//LED indexes range from 0 to 7
-		//Reverse order, since the LEDs should light up left to right
-		for(int i = 7; i >= 0; i--){
-			//Set the register bit to 1 to turn the LED on
-			PORTB |= (1 << i);
+		//Right
+		for(int i = 7; i >= 1; i--){
+			//Set the register bit to 0 to turn the LED on
+			PORTB &= ~(1 << i);
 			//Wait 200 ms
 			_delay_ms(200);
-			//Set the register bit to 0 to turn the LED off
+			//Set the register bit to 1 to turn the LED off
+			PORTB |= (1 << i);
+		}
+		//Left
+		for(int i = 0; i <= 6; i++){
+			//Set the register bit to 0 to turn the LED on
 			PORTB &= ~(1 << i);
+			//Wait 200 ms
+			_delay_ms(200);
+			//Set the register bit to 1 to turn the LED off
+			PORTB |= (1 << i);
 		}
 	}
 }
 
 void aufgabe2(void){
+	//Wait 200 ms = 50000 counts at /64 prescaler
+	//Start Timer 1 in normal mode with /64 prescaler
+	TCCR1B = (1 << CS11) | (1 << CS10);
 	while(1){
 		//LED indexes range from 0 to 7
-		//Reverse order, since the LEDs should light up left to right
-		for(int i = 7; i >= 0; i--){
-			//Set the register bit to 1 to turn the LED on
-			PORTB |= (1 << i);
-			
-			//Wait 200 ms = 50000 counts at /64 prescaler
-			//Overflow value, (2^16 - 1) - 50000 counts
-			TCNT1 = 0x3CAF;
-			//Load Timer1 in normal mode with /64 prescaler, Timer starts immediately
-			TCCR1B = (1 << CS11) | (1 << CS10);
-			//Wait until Timer 1 Overflow is detected
-			while(!(TIFR & (1 << TOV1)));
-			//Deactivate Timer 1
-			TCCR1B = 0;
-			//Clear Timer 1 Overflow flag
-			TIFR &= ~(1 << TOV1);
-			
-			//Set the register bit to 0 to turn the LED off
+		//Right to Left
+		for(int i = 6; i >= 0; i--){
+			//Set the register bit to 0 to turn the LED on
 			PORTB &= ~(1 << i);
+						
+			//Wait until Timer 1 Count value reaches 50000
+			while(TCNT1 <= 0xC34F);
+			//Reset Timer 1 count value
+			TCNT1 = 0;
+			
+			//Set the register bit to 1 to turn the LED off
+			PORTB |= (1 << i);
+		}
+		//Left to Right
+		for(int i = 1; i <= 7; i++){
+			//Set the register bit to 0 to turn the LED on
+			PORTB &= ~(1 << i);
+			
+			//Wait until Timer 1 count value has been reached
+			while(TCNT1 <= 0xC34F);
+			//Reset Timer 1 count value
+			TCNT1 = 0;
+			
+			//Set the register bit to 1 to turn the LED off
+			PORTB |= (1 << i);
 		}
 	}
 }
@@ -80,5 +100,5 @@ c)
 Wie schon erwähnt, wird die CPU durch das Delay blockiert und kann die Zeit nicht nutzen,
 um andere unabhängige Aufgaben zu verrichten. Ein besserer Weg wäre, einen Timer so einzustellen,
 dass beim Erreichen der gewünschten Zeit ein Interrupt gesetzt wird. Dies ermöglicht das Ausführen
-anderer Aufgaben, solange der Timer läuft.
+anderer Aufgaben, solange der Timer läuft, da nicht ständig der Zählwert abgefragt werden muss.
 */
